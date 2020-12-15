4dac9c5f2dad10e136ffa9724ee2de6669d4454b
Errol Cleary <errol.cleary@gmail.com>
2020-12-15 11:29:56 -0800

    Refactor update and pass all tests

--- datastore/index.js
+++ datastore/index.js
@@ -14,7 +14,7 @@ exports.create = (text, callback) => {
     } else {
       fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err) => {
         if (err) {
-          throw ('error writing todo item');
+          callback(err);
         } else {
           callback(null, { id, text });
         }
@@ -58,13 +58,19 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, { id, text });
+        }
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
